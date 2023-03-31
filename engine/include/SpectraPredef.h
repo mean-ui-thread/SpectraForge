9900057e31923292b9717d1f95f08a1b79688913
Mathieu-Andre Chiasson <mchiasson@gmail.com>
2023-03-30 23:36:54 -0400

    More files

--- engine/include/SpectraPredef.h
+++ engine/include/SpectraPredef.h
@@ -1,16 +1,23 @@
 /*******************************************************************************
  * Copyright (c) 2023 Mean UI Thread
- * All rights reserved.
  *
- * NOTICE:  All information contained herein is the property of Mean UI Thread.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * Many of the intellectual and technical concepts contained herein are
- * proprietary to Mean UI Thread, are protected as trade secrets, and are made
- * available only to Mean UI Thread licensees for their internal use. Any
- * reproduction or dissemination of this software is strictly forbidden unless
- * prior written permission is obtained from Mean UI Thread.
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
  *
- * 'SpectraForge' is a registered trademark of Mean UI Thread
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  ******************************************************************************/
 
 #ifndef SPECTRAPREDEF_H
